059e5cab426d3380f1e9d827cafaa25f8781265e
Daniela Leistikow <daniela.leistikow@MacBook-yeah.fritz.box>
2021-01-31 21:55:21 +0100

    landing page finalized

---
+++ source/README.md
@@ -0,0 +1,11 @@
+# Landing Page Project
+
+Udacity Front End Web Developer Nanodegree - Project 2: Landing Page
+
+- Visit: https://danielaleis.github.io/landingpage
+
+My project is the homepage of a bakery that offers delicious cookies.
+
+The objective is to show basic knowledge of Javascript by making a homepage interactive. This entails a navbar being built dynamically. Also the sections need to light up to show which one of them is currently being viewed by the user. When navigating the sections per click, the scroll there needs to be smooth, instead of the default jump.   
+
+
---
+++ source/js/app.js
@@ -0,0 +1,89 @@
+// Creates the navbar dynamically
+
+const navList = document.querySelector('.navbar__menu');
+const navMenu = [
+    'products',
+    'bake',
+    'cookies',
+    'about'
+];
+
+const list = document.createElement('ul');
+
+let i = 0;
+navMenu.forEach(function (listDynamic) {
+    const listElement = document.createElement('li');
+    const listLink = document.createElement('a');
+    listLink.innerHTML = navMenu[i];
+    listLink.setAttribute('href', `#${navMenu[i]}`);
+    listLink.setAttribute('class', 'menu__link');
+    listElement.appendChild(listLink);
+    list.appendChild(listElement);
+    i = i + 1;
+})
+// Add menu to the DOM
+
+navList.appendChild(list);
+
+// Logic for button to expand menu in smallest resolution
+
+document.getElementById("burger").addEventListener('click', () => {
+    if (document.getElementsByClassName("navbar__menu")[0].classList.contains("hidden")) {
+        document.getElementsByClassName("navbar__menu")[0].classList.remove("hidden");
+    } else {
+        document.getElementsByClassName("navbar__menu")[0].classList.add("hidden");
+    }
+});
+
+// Show menu correctly when resizing window
+
+function manageHiddenStatus() {
+    let w = document.documentElement.clientWidth;
+    if (w <= 599) {
+        if (document.getElementsByClassName("navbar__menu")[0].classList.contains("hidden")) {
+            document.getElementsByClassName("navbar__menu")[0].classList.remove("hidden");
+        } else {
+            document.getElementsByClassName("navbar__menu")[0].classList.add("hidden");
+        }
+    } else {
+        if (document.getElementsByClassName("navbar__menu")[0].classList.contains("hidden")) {
+            document.getElementsByClassName("navbar__menu")[0].classList.remove("hidden");
+        }
+    }
+}
+
+window.addEventListener("resize", manageHiddenStatus);
+
+
+// Listens for scroll-event to get current position
+
+const sections = document.querySelectorAll("section[id]");
+
+document.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset;
+    sections.forEach(current => {
+        const height = current.offsetHeight;
+        const top = current.offsetTop;
+        sectionId = current.getAttribute("id");
+        // Manages active class
+        if (
+            scrollTop > top &&
+            scrollTop <= top + height
+        ) {
+            document.querySelector("a[href*=" + sectionId + "]").classList.add("active");
+        } else {
+            document.querySelector("a[href*=" + sectionId + "]").classList.remove("active");
+        }
+    });
+})
+
+// Prevents the default jump and adds smooth scroll to anchor
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
